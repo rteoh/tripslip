6903a3d37a339a71ee7b879a52d1bf1a09efffd0
rteoh <43252547+rteoh@users.noreply.github.com>
2019-09-23 11:33:44 -0700

    Update con.php

--- website/include/con.php
+++ website/include/con.php
@@ -1,7 +1,12 @@
 <?
 
-$con = mysqli_connect("localhost", "tripslip_admin", "SimpelR@T935!", "tripslip_db") or die ("Connection could not be established.");
 
+// Connect to MySQL Database
+$con = mysqli_connect("localhost", "tripslip_admin", "SimpelR@T935!", "tripslip_db") or die ("Connection could not be established.");
 $con->set_charset("utf8");
+
+// Yelp API Key (DO NOT MODIFY)
+$api_key = "vJ-B-0aITbCuUsV1zw0Trm4AsnYl1vCTXS_nzai9rfpwNCD67ywLba8L5yigHqGa-hudmzDuuja3ha1htc6bKmlC4WOrTjiFcMgjxdfNCIUFZz_JYaO3r7BvARjyXXYx";
+
 
 ?>

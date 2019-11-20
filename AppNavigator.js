29d9016412b0de63754efcc3d9fd9af64f0256fa
Marco Botello <marco.botello@csu.fullerton.edu>
2019-11-20 15:38:28 -0800

    Add files via upload

---
+++ AppNavigator.js
@@ -0,0 +1,30 @@
+import { createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import Home from './screens/Home';
+import searchResult from './screens/searchResult';
+import Profile from './screens/Profile';
+
+const AppNavigator = createStackNavigator({
+  Home: { screen: Home },
+  searchResult: { screen: searchResult},
+  Profile: {screen: Profile},
+
+});
+const App=createAppContainer(AppNavigator);
+
+//const DrawerNavigation = StackNavigator({
+//  DrawerStack: { screen: DrawerStack }
+//}, {
+//  headerMode: 'float',
+//  navigationOptions: ({navigation}) => ({
+//    headerStyle: {backgroundColor: '#4C3E54'},
+//    title: 'Welcome!',
+//    headerTintColor: 'white',
+//  })
+//});
+//const DrawerStack=DrawerNavigator({
+//                                  screen1: {screen:Screen1},
+//                                  screen2: {screen:Screen2 },
+//                                  });
+
+export default App;
